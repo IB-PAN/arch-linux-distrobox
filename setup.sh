21b594206a0d12561c25f3161b32e2259b0010a7
Michał Kula <148193449+miku-orb@users.noreply.github.com>
2026-01-12 16:35:39 +0100

    fix?

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ EOF
 # Chaotic AUR
 pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key 3056513887B78AEB
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 tee -a /etc/pacman.conf <<'EOF'
 [chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist
